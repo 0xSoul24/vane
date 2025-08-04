841cd650d92e4b872f9c90bbfeb157474c8632c9
0xSoul <31786518+0xSoul24@users.noreply.github.com>
2025-08-04 15:32:06 -0500

    Replace deprecated "RegistryFreezeEvent" with "RegistryComposeEvent"

--- vane-enchantments/src/main/java/org/oddlama/vane/enchantments/enchantments/registry/WingsRegistry.java
+++ vane-enchantments/src/main/java/org/oddlama/vane/enchantments/enchantments/registry/WingsRegistry.java
@@ -1,17 +1,20 @@
 package org.oddlama.vane.enchantments.enchantments.registry;
 
+import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.data.EnchantmentRegistryEntry;
-import io.papermc.paper.registry.event.RegistryFreezeEvent;
+import io.papermc.paper.registry.event.RegistryComposeEvent;
 import io.papermc.paper.registry.keys.ItemTypeKeys;
 import java.util.List;
+import net.kyori.adventure.key.Key;
 import org.bukkit.enchantments.Enchantment;
 import org.oddlama.vane.enchantments.CustomEnchantmentRegistry;
 
 public class WingsRegistry extends CustomEnchantmentRegistry {
 
-    public WingsRegistry(RegistryFreezeEvent<Enchantment, EnchantmentRegistryEntry.Builder> freezeEvent) {
-        super("wings", List.of(ItemTypeKeys.ELYTRA), 4);
-        this.exclusive_with(List.of(typedKey("wings")));
-        this.register(freezeEvent);
+    public WingsRegistry(RegistryComposeEvent<Enchantment, EnchantmentRegistryEntry.Builder> composeEvent) {
+        super("wings", List.of(ItemTypeKeys.ELYTRA), 5);
+        this.exclusive_with(List.of(TypedKey.create(RegistryKey.ENCHANTMENT, Key.key(NAMESPACE, "angel"))));
+        this.register(composeEvent);
     }
 }
